ea76f5a8934da009f2683b99b065a60386ad974c
Tomas Petricek <tomas@tomasp.net>
2013-04-15 04:45:50 +0100

    Add RPI talk (for reference)

---
+++ Talks 2013/F# Data (RPI)/code/Interactive.fsx
@@ -0,0 +1,9 @@
+﻿#r "packages/FSharp.Data.1.0.13/lib/net40/FSharp.Data.dll"
+#r "System.Xml.Linq.dll"
+
+open FSharp.Data
+
+type Demo = XmlProvider<"demo.xml">
+let demo = Demo.Load("demo.xml")
+
+demo.Author.Name
