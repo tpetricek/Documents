4db0d3a73cdd7f75db9649e45f4e390c500a718b
Tomas Petricek <tomas@tomasp.net>
2013-12-11 16:46:40 +0200

    Adding Data science BuildStuff talk

---
+++ Talks 2013/Data Science (BuildStuff)/start/FsLab/1 The World.fsx
@@ -0,0 +1,40 @@
+﻿#load @"..\packages\FsLab.0.0.6-beta\FsLab.fsx"
+open Deedle
+open FSharp.Data
+open FSharp.Net
+
+// ----------------------------------------------------------------------------
+// Get information about countries 
+// ----------------------------------------------------------------------------
+
+// TODO: Compare Lithuanian tertiary school enrollment (male & female)
+// DEMO: Get a list of countries
+// TODO: Compare a list of countries
+
+// ----------------------------------------------------------------------------
+// Get temperature information for all the countries
+// ----------------------------------------------------------------------------
+
+// TODO: Get capital cities from the WorldBank
+
+// TODO: Get weather information using
+// http://api.openweathermap.org/data/2.5/weather?units=metric&q=Prague
+
+// TODO: Print country & temperature, write getTemp
+// TODO: Get temperature in capitals as a series
+
+// ----------------------------------------------------------------------------
+// Get additional information from the world bank
+// ----------------------------------------------------------------------------
+
+// TODO: Get GDP of Czech Republic in 2000
+// TODO: Build data frame with temperatures (info)
+
+// TODO: Add GDP column for all countries
+// DEMO: Add additional columns
+
+// ----------------------------------------------------------------------------
+// Visualize data with the R type provider
+// ----------------------------------------------------------------------------
+
+// TODO: Visualize data using 'graphics' 'plot'
---
+++ Talks 2013/Data Science (BuildStuff)/start/WorldBank/Page.fs
@@ -0,0 +1,46 @@
+﻿[<ReflectedDefinition>]
+module Program
+
+open FunScript
+open FSharp.Data
+
+type lib = TypeScript.Api<"C:/Typings/lib.d.ts">
+
+// DEMO: WorldBank type Provider & typing for JQuery
+// DEMO: Create a list of countries (countries : unit -> Country[])
+
+let main() = 
+  // DEMO: Create checkboxes for all the countries
+  // DEMO: Add rendering function
+  // DEMO: Add event handlers to check boxes
+  ()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// ------------------------------------------------------------------
+let components = 
+  FunScript.Data.Components.DataProviders @ 
+  FunScript.Interop.Components.all
+do Runtime.Run(components=components, directory="Web")
